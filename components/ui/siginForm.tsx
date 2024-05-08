a2c4e9d53185cb7a629332645afe259b93919080
youngtiger1 <103368810+youngtiger1@users.noreply.github.com>
2024-05-08 09:51:15 +0300

    Update siginForm.tsx

--- components/ui/siginForm.tsx
+++ components/ui/siginForm.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { signIn, useSession } from "next-auth/react";
+import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
+import { LiaSpinnerSolid } from "react-icons/lia";
 import { toast } from "react-hot-toast";
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 type FormType = "signin" | "signup";
 
-const SigninForm = ({ formType }: { formType: FormType }) => {
+const SiginForm = ({ formType }: { formType: FormType }) => {
   const [userInput, setUserInput] = useState({
     name: "",
     email: "",
@@ -19,30 +21,29 @@ const SigninForm = ({ formType }: { formType: FormType }) => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  useEffect(() => {
-    if (session?.user) {
-      pushUser();
-    }
-  }, [session, router]);
-
   const pushUser = async () => {
+    toast(() => (
+      <span className=" animate-spin">
+        <LiaSpinnerSolid />
+      </span>
+    ));
     try {
       const response = await axios.put("/api/users/signup", session?.user);
       if (response.data === "Created New Account") {
         router.push("/setup-profile");
-      } else {
-        router.push("/home");
       }
     } catch (error) {
       if (!session?.user?.image) {
         router.push("/setup-profile");
-      } else {
-        router.push("/home");
-      }
+      } else router.push("/home");
     }
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  if (session?.user) {
+    pushUser();
+  }
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     if (formType === "signin" && userInput.email && userInput.password) {
@@ -59,31 +60,33 @@ const SigninForm = ({ formType }: { formType: FormType }) => {
       userInput.password &&
       userInput.email
     ) {
-      try {
-        const response = await axios.put("/api/users/signup", userInput);
-        if (response.data === "Created New Account") {
-          router.push("/signin");
-        }
-      } catch (error) {
-        toast.error(
-          `Account with email "${userInput.email}" already exists`
-        );
-      }
-    } else {
-      toast.error("Fill all details");
-    }
+      toast
+        .promise(axios.put("/api/users/signup", userInput), {
+          loading: "Creating new account...",
+          success: <p>Successfully created account</p>,
+          error: (
+            <p>
+              Account with email <b>"{userInput.email}"</b> already exists
+            </p>
+          ),
+        })
+        .then((resp) => {
+          if (resp.data === "Created New Account") {
+            router.push("/signin");
+          }
+        });
+    } else toast.error("Fill all details");
   };
-
   return (
     <div className="flex flex-col items-center justify-center gap-4 p-2">
       <form
-        className="flex flex-col items-center justify-center gap-4 p-2"
+        className=" flex flex-col items-center justify-center gap-4 p-2"
         onSubmit={(e) => handleSubmit(e)}
       >
         {formType === "signup" && (
           <input
             type="text"
-            className="min-w-0 rounded-lg border-2 border-darkGray bg-transparent p-2"
+            className=" min-w-0 rounded-lg border-2 border-darkGray bg-transparent p-2"
             placeholder="Full Name"
             value={userInput.name}
             onChange={(e) =>
@@ -96,7 +99,7 @@ const SigninForm = ({ formType }: { formType: FormType }) => {
         )}
         <input
           type="email"
-          className="min-w-0 rounded-lg border-2 border-darkGray bg-transparent p-2"
+          className=" min-w-0 rounded-lg border-2 border-darkGray bg-transparent p-2"
           placeholder="Email"
           value={userInput.email}
           onChange={(e) =>
@@ -108,7 +111,7 @@ const SigninForm = ({ formType }: { formType: FormType }) => {
         />
         <input
           type="password"
-          className="min-w-0 rounded-lg border-2 border-darkGray bg-transparent p-2"
+          className=" min-w-0 rounded-lg border-2 border-darkGray bg-transparent p-2"
           placeholder="Password"
           value={userInput.password}
           onChange={(e) =>
@@ -120,14 +123,14 @@ const SigninForm = ({ formType }: { formType: FormType }) => {
         />
         <button
           type="submit"
-          className="mt-4 rounded-lg bg-lightGray p-2 px-14 font-semibold dark:bg-darkGray md:w-full"
+          className=" mt-4 rounded-lg bg-lightGray p-2 px-14 font-semibold dark:bg-darkGray md:w-full"
         >
           {formType === "signup" ? "Create Account" : "Sign in"}
         </button>
       </form>
-      <p className="-my-3 text-xl font-semibold">or</p>
+      <p className=" -my-3 text-xl font-semibold">or</p>
       <button
-        className="flex items-center gap-2 rounded-md bg-extraLightGray p-2 font-bold shadow-xl dark:bg-lightTheme dark:text-darkTheme md:w-full"
+        className=" flex items-center gap-2 rounded-md bg-extraLightGray p-2 font-bold shadow-xl dark:bg-lightTheme dark:text-darkTheme md:w-full"
         onClick={() => signIn("google")}
       >
         <FcGoogle />
@@ -137,16 +140,15 @@ const SigninForm = ({ formType }: { formType: FormType }) => {
         {formType === "signup"
           ? "Already have an account, "
           : "Don't have an Account , "}
-        <a
+
+        <Link
           href={formType === "signup" ? "/signin" : "/signup"}
           className="text-blue-500 underline dark:text-blue-300"
         >
           {formType === "signup" ? "Sign in" : "sign up"}
-        </a>
+        </Link>
       </p>
     </div>
   );
 };
-
-export default SigninForm;
-          
+export default SiginForm;
